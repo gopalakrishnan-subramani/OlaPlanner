8c4f7d696f0f613b3027b4ab804ada9d9cfade45
gopalakrishnan.subramani@gmai.com <gopalakrishnan.subramani@gmai.com>
2015-02-07 17:42:01 +0530

    added direction

--- www/js/database.js
+++ www/js/database.js
@@ -4,15 +4,68 @@ angular.module('starter.database', [])
         //Model Class, don't disturb now 
         var Plan = Parse.Object.extend("Plan");
 
-                // A Collection containing all instances of TestObject.
+        // A Collection containing all instances of Plan.
         var PlanCollection = Parse.Collection.extend({
           model: Plan
+        });
+
+
+        //Model Class, don't disturb now 
+        var Trip = Parse.Object.extend("Trip");
+
+                // A Collection containing all instances of Trip.
+        var TripCollection = Parse.Collection.extend({
+          model: Trip
         });
 
         //use collect.get('id') or collection.at(index) or collection.each(function(item))
 
 
         console.info("In LiveStream service");
+
+        this.newTrip = function() {
+          return new Trip();
+        }
+
+        this.getTrips = function(planId) {
+          var deferred = $q.defer();
+          //FIXME: Add planId for filtering
+          var collection = new TripCollection();
+                collection.fetch({
+                  success: function(collection) {
+                     return deferred.resolve(collection);
+                  },
+                  error: function(collection, error) {
+                    // The collection could not be retrieved.
+                    return deferred.reject(error);
+                  }
+            });
+
+          return deferred.promise;
+        };
+
+
+        this.getTrip = function(id){
+            var deferred = $q.defer();
+
+            var query = new Parse.Query(Trip);
+
+            query.get(id, {
+                  success: function(plan) {
+                    alert('got the document ' + plan.get('source') + ' ' + plan.get('destination'));
+
+                    // The object was retrieved successfully.
+                    return deferred.resolve(plan);
+                  },
+                  error: function(object, error) {
+                    // The object was not retrieved successfully.
+                    // error is a Parse.Error with an error code and message.
+                     return deferred.reject(error);
+                  }
+            });
+           return deferred.promise;
+      };
+
 
         this.newPlan = function() {
           return new Plan();
