cbffa0d413dd8965a39e66cc99b73db7bb75bd7b
Pulak <bhattacharyyapulak@gmail.com>
2015-02-07 20:01:34 +0530

    Trip Details with Google Distance Matrix

--- www/js/tripDetails.js
+++ www/js/tripDetails.js
@@ -1,4 +1,74 @@
 angular.module('starter.tripDetails', [])
-.controller('TripDetailsCtrl', ['$scope', '$http', function ($scope, $http) {
+.service('DistanceServiceMatrix', function ($http, $q) {
+
+	var map;
+	var origin;
+	var destination;
+	var deferred;
+
+	function callback (response, status) {
+	  if (status != google.maps.DistanceMatrixStatus.OK) {
+	    deferred.resolve(status);
+	  } else {
+	  	deferred.resolve(response);
+	  }
+	}
+
+	this.calculateDistances = function (_sourceDes) {
+		deferred = $q.defer();
+
+		origin = _sourceDes.source;
+		destination = _sourceDes.destination;
+
+		var service = new google.maps.DistanceMatrixService();
+
+	  service.getDistanceMatrix(
+	    {
+	      origins: [origin],
+	      destinations: [destination],
+	      travelMode: google.maps.TravelMode.DRIVING,
+	      unitSystem: google.maps.UnitSystem.METRIC,
+	      avoidHighways: false,
+	      avoidTolls: false
+	    }, callback);
+
+	  return deferred.promise;
+	}
+})
+.controller('TripDetailsCtrl', ['$scope', '$http', 'DistanceServiceMatrix', function ($scope, $http, DistanceServiceMatrix) {
+
+	$scope.matrixData;
+
+	//Hard Coding for now Source and Destination
+	var _inputs = {
+		source: "bangalore",
+		destination: "mysore"
+	};
+
+	var _parsedData,
+			_matrixItems = [];
+
+	//Parse JSON response coming from service
+	function parseResponse (response) {
+		var _rows = response.rows,
+				length = _rows.length;
+				
+		for (var i = 0; i < length; i++) {
+			_elements = _rows[i]["elements"];
+			for (var j = 0; j < _elements.length; j ++) {
+				_matrixItems.push(_elements[j]);				
+			}
+		}
+
+		$scope.matrixData = _matrixItems;
+		console.log($scope.matrixData);
+	}
+
+	DistanceServiceMatrix.calculateDistances(_inputs).then(function (response) {
+		//Parse the response data to pass it to template
+		parseResponse (response);
+	}, function (reason) {
+		console.log(reason);
+	});
 
 }]);
